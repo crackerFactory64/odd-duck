d16030daee256fbf3986f8e76bd211be96ac2607
Lee Mander <lee.mander@gmail.com>
2023-10-02 14:31:33 +0100

    Create constructor function and populate array

---
+++ app.js
@@ -0,0 +1,28 @@
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  clicks = 0;
+  views = 0;
+}
+
+const products = [
+  new Product("R2D2 Bag", "./assets/images/bag.jpg"),
+  new Product("Banana Slicer", "./assets/images/bananna.jpg"),
+  new Product("Tablet Holder", "./assets/images/bathroom.jpg"),
+  new Product("Toeless Wellies", "./assets/images/boots.jpg"),
+  new Product("Breakfast Oven", "./assets/images/breakfast.jpg"),
+  new Product("Meatball Bubblegum", "./assets/images/bubblegum.jpg"),
+  new Product("Lumpy Chair", "./assets/images/chair.jpg"),
+  new Product("Cthulhu Doll", "./assets/images/cthulhu.jpg"),
+  new Product("Dog Quacker", "./assets/images/dog-duck.jpg"),
+  new Product("Dragon Meat", "./assets/images/dragon.jpg"),
+  new Product("Pen Cutlery", "./assets/images/pen.jpg"),
+  new Product("Pet Sweepers", "./assets/images/pet-sweep.png"),
+  new Product("Pizza Scissors", "./assets/images/scissors.jpg"),
+  new Product("Shark Sleeping Bag", "./assets/images/shark.jpg"),
+  new Product("Baby Sweeper", "./assets/images/sweep.png"),
+  new Product("Tauntaun Sleeping Bag", "./assets/images/tauntaun.jpg"),
+  new Product("Unicorn Meat", "./assets/images/unicorn.jpg"),
+  new Product("Infinite Watering Can", "./assets/images/water-can.jpg"),
+  new Product("Egg Wine Glass", "./assets/images/wine-glass.jpg"),
+];
